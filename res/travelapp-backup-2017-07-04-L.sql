e66610170c5c14b12f068498030fe693364335db
Rômulo Berri <romuloberri@gmail.com>
2017-07-06 18:11:28 -0300

    SQL to create all database, initial version

--- res/travelapp-backup-2017-07-04-L.sql
+++ res/travelapp-backup-2017-07-04-L.sql
@@ -1,157 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 05-Jul-2017 às 12:45
--- Versão do servidor: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+/* = USER ============================== */
+CREATE TABLE `dreamplace` (
+  `id_dreamplace` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_dreamplace`),
+  KEY `fk_dreamplaces_usuario1_idx` (`id_usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+CREATE TABLE `interests` (
+  `id_interests` int(11) NOT NULL,
+  `description` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_interests`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `travelapp`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `config`
---
-
-CREATE TABLE `config` (
-  `id_config` int(11) NOT NULL,
-  `descricao` varchar(100) DEFAULT NULL,
-  `trocasenhatempo` char(1) DEFAULT NULL,
-  `tempotrocasenha` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `config`
---
-
-INSERT INTO `config` (`id_config`, `descricao`, `trocasenhatempo`, `tempotrocasenha`) VALUES
-(1, 'Troca da Senha', 'S', 360);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `item`
---
-
-CREATE TABLE `item` (
-  `id_item` bigint(20) UNSIGNED NOT NULL,
-  `categoria` varchar(2) COLLATE utf8_bin NOT NULL,
-  `titulo` varchar(100) COLLATE utf8_bin NOT NULL,
-  `texto` text COLLATE utf8_bin NOT NULL,
-  `datacadastro` date DEFAULT NULL,
-  `observacao` text COLLATE utf8_bin,
-  `notarodape` text COLLATE utf8_bin
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Extraindo dados da tabela `item`
---
-
-INSERT INTO `item` (`id_item`, `categoria`, `titulo`, `texto`, `datacadastro`, `observacao`, `notarodape`) VALUES
-(6, '1', 'Conclusion', '<p>Foram calculadas as verbas constantes das decisÃµes judiciais demonstradas nos anexos.</p>\r\n<table style="margin-left: auto; margin-right: auto;">\r\n<thead>\r\n<tr>\r\n<th>Item</th>\r\n<th>DescriÃ§Ã£o</th>\r\n<th>Valor</th>\r\n</tr>\r\n</thead>\r\n<tbody>\r\n<tr>\r\n<td>1</td>\r\n<td>Total da Verba</td>\r\n<td>R$ 186.828,14</td>\r\n</tr>\r\n<tr>\r\n<td>2</td>\r\n<td>Valor corrigido</td>\r\n<td>R$ 199.009,26</td>\r\n</tr>\r\n<tr>\r\n<td>3</td>\r\n<td>Juros</td>\r\n<td>R$ 85.882,87</td>\r\n</tr>\r\n</tbody>\r\n<tfoot>\r\n<tr>\r\n<td colspan="">VALOR TOTAL (2+3)</td>\r\n<td>R$ 284.892,13</td>\r\n</tr>\r\n</tfoot>\r\n</table>\r\n<p>ReferÃªncia de CÃ¡lculo:</p>\r\n<ul>\r\n<li>Anexo I â€“ Resumo Geral</li>\r\n</ul>', '2016-06-17', NULL, '<p>asdvas</p>');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `log`
---
-
-CREATE TABLE `log` (
-  `id_log` bigint(20) UNSIGNED NOT NULL,
-  `descricao` text COLLATE utf8_bin NOT NULL,
-  `usuario` varchar(50) COLLATE utf8_bin DEFAULT NULL,
-  `datahora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `id_owner` int(11) DEFAULT NULL,
-  `controller` varchar(100) COLLATE utf8_bin NOT NULL,
-  `tipo` int(11) NOT NULL,
-  `act` varchar(50) COLLATE utf8_bin NOT NULL,
-  `ip` varchar(70) COLLATE utf8_bin NOT NULL,
-  `acao` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `permissao`
---
-
-CREATE TABLE `permissao` (
-  `id_permissao` int(11) NOT NULL,
-  `id_processo` int(11) DEFAULT NULL,
-  `ver` char(1) DEFAULT NULL,
-  `inserir` char(1) DEFAULT NULL,
-  `excluir` char(1) DEFAULT NULL,
-  `editar` char(1) DEFAULT NULL,
-  `id_usuario` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `permissao`
---
-
-INSERT INTO `permissao` (`id_permissao`, `id_processo`, `ver`, `inserir`, `excluir`, `editar`, `id_usuario`) VALUES
-(1, 1, 'S', 'S', 'S', 'S', 1),
-(71, 14, 'S', 'S', NULL, NULL, 4),
-(72, 17, 'S', 'S', NULL, 'S', 3),
-(69, 17, 'S', NULL, NULL, NULL, 4),
-(68, 16, 'S', 'S', 'S', 'S', 2),
-(67, 15, 'S', 'S', NULL, 'S', 3),
-(66, 14, 'S', 'S', 'S', 'S', 3),
-(65, 3, 'S', 'S', 'S', 'S', 3),
-(64, 14, 'S', 'S', 'S', 'S', 2),
-(63, 3, 'S', 'S', 'S', 'S', 2),
-(62, 3, 'S', NULL, NULL, NULL, 34),
-(61, 3, 'S', 'S', 'S', 'S', 33);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `processo`
---
-
-CREATE TABLE `processo` (
-  `id_processo` int(11) NOT NULL,
-  `nome` varchar(60) DEFAULT NULL,
-  `descricao` varchar(50) DEFAULT NULL,
-  `controladores` varchar(160) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `processo`
---
-
-INSERT INTO `processo` (`id_processo`, `nome`, `descricao`, `controladores`) VALUES
-(1, 'ALL', 'Acesso total ao sistema', NULL),
-(3, 'PROC_CAD_LaL', 'Lunch and Learn', 'course, bookedcourse, index, usuario'),
-(16, 'PROC_CAD_APPROVE_EDU', 'Approve Educators', 'index, usuario, course'),
-(15, 'CHANGE_REALDATE', 'Change de Confimed Date', NULL),
-(17, 'PROC_CAD_USERS', 'Users Edit', 'usuario'),
-(14, 'PROC_CAD_BOOKED', 'Booked Lunch and Learn', 'bookedcourse, index, course, usuario'),
-(163, NULL, NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
+CREATE TABLE `travelertype` (
+  `id_travelertype` int(11) NOT NULL,
+  `description` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_travelertype`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
+/* fields from framework */
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `loginuser` varchar(25) DEFAULT NULL,
   `nomecompleto` varchar(35) DEFAULT NULL,
+  `lastname` varchar(35) DEFAULT NULL,
   `senha` varchar(32) DEFAULT NULL,
   `datasenha` date DEFAULT NULL,
   `tipo` varchar(7) DEFAULT NULL,
@@ -171,93 +45,307 @@ CREATE TABLE `usuario` (
   `paginainicial` varchar(100) DEFAULT NULL,
   `telephone` varchar(25) DEFAULT NULL,
   `Photo` varchar(200) DEFAULT NULL,
-  `approved` char(1) DEFAULT 'N'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `approved` char(1) DEFAULT 'N',
+/* end fields from framework */
+  profileurl varchar(255),
+  actualcity varchar(50),
+  actualcountry varchar(50),
+  liveincity varchar(50),
+  liveincountry varchar(50),
+  birthdate date,
+  gender enum('M','F','O'),
+  relationship enum('s','m','ir','e','cu','dp','or','ic','s','d','w') comment 'single, married, in a relationship, engaged, in a civil union, in a domestic partnership, in an open relationship, it is complicated, separated, divorced, widowed',
+  bio varchar(140),
+  instagram varchar(45),
+  twitter varchar(45),
+  facebook varchar(45),
+  occupation varchar(60),
+  dreamjob varchar(60),
+  calendartype int,
+  traveledto varchar(255),
+  lastvisitdate date,
+  lastvisitlocation varchar(255),
+  lastvisittext varchar(255),
+  nexttripdate date,
+  nexttriplocation varchar(255),
+  nexttriptext varchar(255),
+  education varchar(100),
+  hometowncity varchar(50),
+  hometowncountry varchar(50),
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `usuario_loginuser_key` (`loginuser`)
+) ENGINE=MyISAM AUTO_INCREMENT=49 DEFAULT CHARSET=utf8;
+/* Main photo and cover photo won’t be on database, will be a folder structure based on id_usuario */
 
---
--- Extraindo dados da tabela `usuario`
---
 
-INSERT INTO `usuario` (`id_usuario`, `loginuser`, `nomecompleto`, `senha`, `datasenha`, `tipo`, `email`, `senhaemail`, `assinaturaemail`, `smtp`, `porta`, `grupo`, `ativo`, `excluivel`, `editavel`, `id_empresa`, `idexterno`, `dificuldade`, `trocasenhatempo`, `paginainicial`, `telephone`, `Photo`, `approved`) VALUES
-(1, 'admin', 'Administrador', '28335c822634cc5f5992415058957371', '2016-12-20', 'user', 'admin', NULL, NULL, NULL, NULL, 1, 'S', 'N', 'N', 1, NULL, NULL, 'S', 'index', NULL, NULL, 'S');
+CREATE TABLE `userinterests` (
+  `id_usuario` int(11) NOT NULL,
+  `id_interests` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_interests`),
+  KEY `fk_userinterests_usuario_idx` (`id_usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+CREATE TABLE `usertravelertype` (
+  `id_usuario` int(11) NOT NULL,
+  `id_travelertype` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_travelertype`),
+  KEY `fk_usertravelertype_travelertype_idx` (`id_travelertype`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Indexes for table `config`
---
-ALTER TABLE `config`
-  ADD PRIMARY KEY (`id_config`);
+CREATE TABLE `friend` (
+  `id_usuario` int(11) NOT NULL,
+  `id_friend` int(11) NOT NULL,
+  `asked_at` datetime DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `accepted_at` datetime DEFAULT NULL,
 
---
--- Indexes for table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id_item`),
-  ADD UNIQUE KEY `id_laudotopico` (`id_item`);
+  PRIMARY KEY `fk_friend_usuario1_idx` (`id_usuario`, `id_friend`),
+  KEY `fk_friends_usuario2_idx` (`id_friend`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Indexes for table `log`
---
-ALTER TABLE `log`
-  ADD UNIQUE KEY `id_log` (`id_log`);
+/* =ACTIVITIES======================== */
 
---
--- Indexes for table `permissao`
---
-ALTER TABLE `permissao`
-  ADD PRIMARY KEY (`id_permissao`),
-  ADD KEY `permissao_id_processo_fkey` (`id_processo`);
+CREATE TABLE `activitytype` (
+  id_activitytype int(11) NOT NULL,
+  activitytypename varchar(30),
+  PRIMARY KEY (`id_activitytype`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Indexes for table `processo`
---
-ALTER TABLE `processo`
-  ADD PRIMARY KEY (`id_processo`);
+CREATE TABLE `currency` (
+  `id_currency` int(11) NOT NULL,
+  `symbol` varchar(4) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_currency`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`),
-  ADD UNIQUE KEY `usuario_loginuser_key` (`loginuser`);
+CREATE TABLE `activity` (
+  id_activity int(11) NOT NULL,
+  activityname varchar(45) NOT NULL,
+  id_activitytype int(11) NOT NULL,
+  activitysupply varchar(500),
+  start_at datetime,
+  end_at datetime,
+  id_currency int(11),
+  price decimal(13,2),
+  address varchar(150),
+  city varchar(50),
+  country varchar(50),
+  lat decimal(9,6),
+  lng decimal(9,6),
+  geoloc point,
+  description varchar(500),
+  dresscode varchar(150),
+  PRIMARY KEY (`id_activity`),
+  KEY `fk_activity_activitytype_idx` (`id_activitytype`),
+  KEY `fk_activity_currency_idx` (`id_currency`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- AUTO_INCREMENT for dumped tables
---
+CREATE TABLE `invitation` (
+  `id_invitation` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `firstname` varchar(35) DEFAULT NULL,
+  `lastname` varchar(35) DEFAULT NULL,
+  emailsent_at datetime,
+  PRIMARY KEY (`id_invitation`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- AUTO_INCREMENT for table `config`
---
-ALTER TABLE `config`
-  MODIFY `id_config` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `item`
---
-ALTER TABLE `item`
-  MODIFY `id_item` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `log`
---
-ALTER TABLE `log`
-  MODIFY `id_log` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `permissao`
---
-ALTER TABLE `permissao`
-  MODIFY `id_permissao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
---
--- AUTO_INCREMENT for table `processo`
---
-ALTER TABLE `processo`
-  MODIFY `id_processo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=164;
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `activityuser` (
+  `id_activityuser` int(11) NOT NULL,
+  `id_activity` int(11) NOT NULL,
+  `id_usuario` int(11), /* null when it is a not registered user */
+  `status` enum('i', 'w', 'o', 'c', 'b') NOT NULL comment 'invited(may be not registered), wishlist, owner, confirmed, blocked',
+  `id_invitation` int(11),
+  PRIMARY KEY (`id_activityuser`),
+  KEY `fk_activityuser_activity_idx` (`id_activity`),
+  KEY `fk_activityuser_usuario_idx` (`id_usuario`),
+  KEY `fk_activityuser_invitation_idx` (`id_invitation`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+/* =TRIP======================== */
+
+CREATE TABLE `triptype` (
+  `id_triptype` int(11) NOT NULL,
+  triptypename varchar(25),
+  PRIMARY KEY (`id_triptype`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `trip` (
+  `id_trip` int(11) NOT NULL,
+  tripname varchar(30),
+  publicurl varchar(50),
+  description varchar(500),
+  startdate datetime,
+  enddate datetime,
+  travelmethod varchar(500),
+  inventory varchar(500),
+  notes varchar(500),
+  id_triptype int(11) NOT NULL,
+  PRIMARY KEY (`id_trip`),
+  KEY `fk_trip_triptype_idx` (id_triptype)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tripcity` (
+  `id_tripcity` int(11) NOT NULL,
+  `id_trip` int(11) NOT NULL,
+  startdate datetime,
+  enddate datetime,
+  city varchar(50),
+  country varchar(50),
+  PRIMARY KEY (`id_tripcity`),
+  KEY `fk_tripcity_trip_idx` (`id_trip`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tripuser` (
+  `id_tripuser` int(11) NOT NULL,
+  `id_trip` int(11) NOT NULL,
+  `id_usuario` int(11), /* null when it is a not registered user */
+  `id_invitation` int(11),
+  `status` enum('i','w', 'o', 'c', 'b') NOT NULL comment 'invited(not registered), wishlist, owner, confirmed, blocked',
+  `joined_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_tripuser`),
+  KEY `fk_tripuser_usuario_idx` (`id_usuario`),
+  KEY `fk_tripuser_trip_idx` (`id_trip`),
+  KEY `fk_tripuser_invitation_idx` (`id_invitation`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tripbudget` (
+  `id_tripbudget` int(11) NOT NULL,
+  `id_trip` int(11) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `value` decimal(13,2) DEFAULT NULL,
+  `id_currency` int(11) NOT NULL,
+  PRIMARY KEY (`id_tripbudget`),
+  KEY `fk_tripbudget_trip1_idx` (`id_trip`),
+  KEY `fk_tripbudget_currency1_idx` (`id_currency`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tripactivity` (
+  `id_tripactivity` int(11) NOT NULL,
+  `id_trip` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL comment 'user who suggested',
+  `status` enum('s','c') NOT NULL comment 'suggestion (trip wishlist), confirmed',
+/* in the future this can have a likes table and count */
+  PRIMARY KEY (`id_tripactivity`),
+  KEY `fk_tripactivity_trip_idx` (`id_trip`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tripactivitycomment` (
+  `id_tripactivitycomment` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_trip` int(11) NOT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_tripactivitycomment`),
+  KEY `fk_tripactivitycomment_usuario_idx` (`id_usuario`),
+  KEY `fk_tripactivitycomment_trip_idx` (`id_trip`)
+ ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tripchat` (
+  `id_tripchat` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_trip` int(11) NOT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_tripchat`),
+  KEY `fk_tripchat_usuario1_idx` (`id_usuario`),
+  KEY `fk_tripchat_trip1_idx` (`id_trip`)
+ ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+/*=EVENT================*/
+
+CREATE TABLE `eventtype` (
+  id_eventtype int(11) NOT NULL,
+  description varchar(20) NOT NULL,
+  PRIMARY KEY (`id_eventtype`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `event` (
+  id_event int(11) NOT NULL,
+  id_eventtype int(11) NOT NULL,
+  eventname varchar(30),
+  start_at datetime,
+  end_at datetime,
+  eventsupply varchar(500),
+  id_currency int(11),
+  price decimal(13,2),
+  address varchar(150),
+  city varchar(50),
+  country varchar(50),
+  lat decimal(9,6),
+  lng decimal(9,6),
+  geoloc point,
+  description varchar(500),
+  dresscode varchar(150),
+  PRIMARY KEY (`id_event`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `eventuser` (
+  `id_eventuser` int(11) NOT NULL,
+  `id_event` int(11) NOT NULL,
+  `id_usuario` int(11),
+  `status` enum('i', 'w', 'o', 'c', 'b') NOT NULL comment 'invited(may be not registered), wishlist, owner, confirmed, blocked',
+  `id_invitation` int(11),
+  PRIMARY KEY (`id_eventuser`),
+  KEY `fk_eventuser_usuario_idx` (`id_usuario`),
+  KEY `fk_eventuser_event_idx` (`id_event`),
+  KEY `fk_eventuser_invitation_idx` (`id_invitation`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tripevent` (
+  `id_tripevent` int(11) NOT NULL,
+  `id_trip` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL comment 'user who suggested',
+  `status` enum('s','c') NOT NULL comment 'suggestion (trip wishlist), confirmed',
+  PRIMARY KEY (`id_tripevent`),
+  KEY `fk_tripevent_trip_idx` (`id_trip`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+/* = Framework ================== */
+CREATE TABLE `config` (
+  `id_config` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(100) DEFAULT NULL,
+  `trocasenhatempo` char(1) DEFAULT NULL,
+  `tempotrocasenha` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_config`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `log` (
+  `id_log` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `descricao` text COLLATE utf8_bin NOT NULL,
+  `usuario` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `datahora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_owner` int(11) DEFAULT NULL,
+  `controller` varchar(100) COLLATE utf8_bin NOT NULL,
+  `tipo` int(11) NOT NULL,
+  `act` varchar(50) COLLATE utf8_bin NOT NULL,
+  `ip` varchar(70) COLLATE utf8_bin NOT NULL,
+  `acao` int(11) NOT NULL,
+  UNIQUE KEY `id_log` (`id_log`),
+  KEY `fk_log_usuario1_idx` (`id_owner`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+CREATE TABLE `permissao` (
+  `id_permissao` int(11) NOT NULL AUTO_INCREMENT,
+  `id_processo` int(11) DEFAULT NULL,
+  `ver` char(1) DEFAULT NULL,
+  `inserir` char(1) DEFAULT NULL,
+  `excluir` char(1) DEFAULT NULL,
+  `editar` char(1) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_permissao`),
+  KEY `permissao_id_processo_fkey` (`id_processo`),
+  KEY `fk_permissao_usuario_idx` (`id_usuario`)
+) ENGINE=MyISAM AUTO_INCREMENT=73 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `processo` (
+  `id_processo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(60) DEFAULT NULL,
+  `descricao` varchar(50) DEFAULT NULL,
+  `controladores` varchar(160) DEFAULT NULL,
+  PRIMARY KEY (`id_processo`)
+) ENGINE=MyISAM AUTO_INCREMENT=164 DEFAULT CHARSET=utf8;
+
