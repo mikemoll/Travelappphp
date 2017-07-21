0e245a279e9e0dc9a1a66faa5f60254c623b0c82
Leonardo Danieli <leonardo.danieli@gmail.com>
2017-07-21 17:20:05 -0300

    My Profile - list of the Dreamboard and Trips

--- site/Application/Models/Dreamboard.php
+++ site/Application/Models/Dreamboard.php
@@ -19,9 +19,34 @@ class Dreamboard extends Db_Table {
         $this->a_created_at = date('m/d/Y H:i:s');
     }
 
+    public function getPhotoPath() {
+        if ($this->getId_Activity() != '') {
+            return Activity::makephotoPath($this->getId_Activity(), $this->a_a_photo);
+        } elseif ($this->getId_Event() != '') {
+            return Event::makephotoPath($this->getId_Event(), $this->a_e_photo);
+        } elseif ($this->getId_Place() != '') {
+            return Place::makephotoPath($this->getId_Place(), $this->a_p_photo);
+        }
+    }
+
+    public function getType() {
+        $r = ($this->getId_Activity() != '') ? "Activity" : '';
+        $r .= ($this->getId_Event() != '') ? "Event" : '';
+        $r .= ($this->getId_Place() != '') ? "Place" : '';
+        return $r;
+    }
+
+    public function getName() {
+        $r = ($this->getId_Activity() != '') ? $this->getActivityname() : '';
+        $r .= ($this->getid_event() != '') ? $this->geteventname() : '';
+        $r .= ($this->getId_Place() != '') ? $this->getpname() : '';
+        return $r;
+    }
+
     function readLst($modo = 'obj') {
-        $this->join('activity', 'activity.id_activity = dreamboard.id_activity', 'activityname', 'left');
-        $this->join('event', 'event.id_event = dreamboard.id_event', 'eventname', 'left');
+        $this->join('activity', 'activity.id_activity = dreamboard.id_activity', 'activityname , photo as a_photo, country as address', 'left');
+        $this->join('event', 'event.id_event = dreamboard.id_event', 'eventname, photo as e_photo, address', 'left');
+        $this->join('place', 'place.id_place = dreamboard.id_place', 'name as pname, photo as p_photo, formatted_address as address', 'left');
         parent::readLst($modo);
     }
 
