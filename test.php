b5be2f45e62045fff4fbb13db316d7874ad664bd
Rômulo Berri <romuloberri@gmail.com>
2017-09-01 18:11:14 -0300

    heroku database test

--- test.php
+++ test.php
@@ -6,6 +6,9 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
+echo '<pre>';
+var_dump($server, $username, $password, $db);
+
 $conn = new mysqli($server, $username, $password, $db);
 
 
