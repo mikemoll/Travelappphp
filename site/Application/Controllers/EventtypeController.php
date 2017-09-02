878229e6443b810fda124ec35efd6f2c74e783bc
Leonardo Danieli <leonardo.danieli@gmail.com>
2017-09-02 12:40:09 -0300

    composer - changing the php version to 5.6.19

--- site/Application/Controllers/EventtypeController.php
+++ site/Application/Controllers/EventtypeController.php
@@ -20,9 +20,6 @@ class EventtypeController extends AbstractController {
     }
 
     public function indexAction() {
-
-        phpinfo();
-        die();
         $post = Zend_Registry::get('post');
         $form = new Ui_Form();
         $form->setName($this->FormName);
