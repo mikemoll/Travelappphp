32b88288a67847830c18a8e31719596475a1a8c9
Rômulo Berri <romuloberri@gmail.com>
2017-09-08 12:09:03 -0300

    images hroku-aws

--- site/Application/Controllers/TravelertypeController.php
+++ site/Application/Controllers/TravelertypeController.php
@@ -167,7 +167,7 @@ class TravelertypeController extends AbstractController {
             if (!USE_AWS) {
                 move_uploaded_file($image['tmp_name'], $dest );
             } else {
-                require('../../../../vendor/autoload.php');
+                require(RAIZ_DIRETORY . 'vendor/autoload.php');
                 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
                 $s3 = Aws\S3\S3Client::factory();
                 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
