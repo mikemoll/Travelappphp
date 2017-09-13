977b0724f237068cfad2989028d3ec5aa7ae17e7
Rômulo Berri <romuloberri@gmail.com>
2017-09-13 10:26:41 -0300

    aws test

--- site/Application/Controllers/TravelertypeController.php
+++ site/Application/Controllers/TravelertypeController.php
@@ -167,8 +167,8 @@ class TravelertypeController extends AbstractController {
             move_uploaded_file($image['tmp_name'], $dest );
             if (USE_AWS) {
                 $result = Aws::moveToAWS($dest);
-                if (!$result['success']) {
-                    $br->setAlert('Error!', '<pre>' . print_r($result['message'], true) . '</pre>', '100%', '600');
+                if (!$result->success) {
+                    $br->setAlert('Error!', '<pre>' . print_r($result->message, true) . '</pre>', '100%', '600');
                     $br->send();
                     die();
                 }
